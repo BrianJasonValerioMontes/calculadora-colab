f74f17fb35448be65c58ea83f7d9befd4ca0b05b
LykaonOkami <serlobo4@gmail.com>
2025-04-09 22:07:43 -0600

    agrelafuncion

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 def suma(a, b):
     return a + b
 
-print ("----------CALCULADORA----------")
-print ("Seleccione lo que deseas hacer:\n1-Suma\n")
+def resta(a, b):
+    return a - b 
 
+print("----------CALCULADORA----------")
+print("Seleccione lo que deseas hacer:\n1-Suma\n")
 
-numero = int(input("Ingresa un número: "))
+numero = int(input("Ingresa un numero: "))
 
 if numero == 1:
-    a = int(input("Ingresa el primer número: "))
-    b = int(input("Ingresa el segundo número: "))
+    a = int(input("Ingresa el primer numero: "))
+    b = int(input("Ingresa el segundo numero: "))
     
     resultado = suma(a, b)
-    print ("La suma es: ",resultado)
+    print("La suma es:", resultado)
+
+elif numero == 2:
+    a = int(input("ingresa el primer numero: "))
+    b = int(input("ingresa el segund numero: "))
+    resultado = resta(a,b)
+    print("la esta es:", resultado)
+
+else: 
+    print("opcion no valida.")
